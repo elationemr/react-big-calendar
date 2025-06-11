c413a2387831d09c5843fc82d37d97d0b72f63f0
jerrywithaz <dev@jerrywithaz.com>
2025-06-11 12:48:40 -0500

    ESM

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,20 +1,13 @@
 import * as esbuild from "esbuild";
 import path from "path";
-import fs from 'fs';
 
-const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
-
-const deps = Object.keys(pkg.dependencies || {});
-const peerDeps = Object.keys(pkg.peerDependencies || {});
-
-const allExternals = Array.from(new Set([...deps, ...peerDeps]));
 
 export async function build() {
   await esbuild.build({
     entryPoints: [path.resolve(__dirname, "..", "src/index.js")],
     bundle: true,
     platform: "browser",
-    outfile: path.resolve(__dirname, "..", "lib", "index.esm.js"),
+    outfile: path.resolve(__dirname, "..", "lib", "index.es.js"),
     minify: false,
     external: [
      "react",
